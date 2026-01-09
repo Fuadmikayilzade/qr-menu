3b75d4fc07daebc4b7f61f0b0bd6fa522f8827cb
Fuadmikayilzade <mikayilzade5@gmail.com>
2026-01-09 16:03:29 +0400

    remove basename for vercel

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
+// import React from "react";
+// import { Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Luxbar from './Luxbar/Luxbar';
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Services from "./components/Services";
+// import Contact from "./components/Contact";
+// import Social from './social media/social'
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//     <Router basename="/qr-menu">
+//       <Luxbar />
+//       <Routes>
+//         <Route exact path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/services" element={<Services />} />
+//         {/* <Route path="/projects" element={<Projects />} /> */}
+//         <Route path="/contact" element={<Contact />} />
+//         {/* Yanlış yola daxil olunarsa, Home səhifəsinə yönləndir */}
+//         <Route path="*" element={<Navigate to="/" />} />
+
+//       </Routes>
+//       <Social />
+//     </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
-import { Navigate } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Luxbar from './Luxbar/Luxbar';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Luxbar from "./Luxbar/Luxbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import Social from './social media/social'
+import Social from "./social media/social";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <Router basename="/qr-menu">
-      <Luxbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        {/* <Route path="/projects" element={<Projects />} /> */}
-        <Route path="/contact" element={<Contact />} />
-        {/* Yanlış yola daxil olunarsa, Home səhifəsinə yönləndir */}
-        <Route path="*" element={<Navigate to="/" />} />
+      <Router>
+        <Luxbar />
 
-      </Routes>
-      <Social />
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* Yanlış route olarsa Home-a yönləndir */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+
+        <Social />
+      </Router>
     </div>
   );
 }
